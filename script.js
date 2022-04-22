6decabbd80e1cb67c267b2b137807d4d1b2bf9c0
carolcortes <caroline.ocortes@gmail.com>
2022-04-22 20:26:12 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('Olá, mundo!');
+
+// create random guess
+const rgb = document.getElementById('rgb');
+rgb.innerText = `(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+// create colors
