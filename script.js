0d97e07e961b6f30283faf120f88da0332eba953
carolcortes <caroline.ocortes@gmail.com>
2022-04-25 17:00:41 -0300

    requisito 4

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const rgb = document.getElementById('rgb');
 rgb.innerText = `(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 
 // create colors
+const colors = document.getElementById('colors');
+for (let index = 0; index < 6; index += 1) {
+  const colorDiv = document.createElement('div');
+  colorDiv.className = 'ball';
+  colors.appendChild(colorDiv);
+  const rgbColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+  colorDiv.style.backgroundColor = rgbColor;
+}
+
+// select answer
+const colorOption = document.getElementsByClassName('ball')[Math.floor(Math.random() * 6)];
+colorOption.style.backgroundColor = `rgb${rgb.innerText}`;
+colorOption.id = 'answer';
